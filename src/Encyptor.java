b5fb08a97cba04848fa15547ecd1aae6b0e1b695
Moneil <cam8059@yahoo.com>
2015-03-18 21:56:52 -0500

    Initial Commit

---
+++ src/Encyptor.java
@@ -0,0 +1,13 @@
+
+public class Encyptor {
+
+	public Encyptor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
